3ffd909c833cf988ecc76708c919c5d267008731
zoi <zoi@zoizoi.net>
2021-08-07 18:40:58 +0900

    UMLのdata内のクラスを実装

--- src/main/java/jp/hack/minecraft/hideandseek/data/StageData.java
+++ src/main/java/jp/hack/minecraft/hideandseek/data/StageData.java
@@ -1,4 +1,9 @@
 package jp.hack.minecraft.hideandseek.data;
 
+import org.bukkit.Location;
+import org.bukkit.WorldBorder;
+
 public class StageData {
+    private Location stageCenter;
+    private WorldBorder stageBorder;
 }
