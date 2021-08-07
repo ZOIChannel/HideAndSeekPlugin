a10d3c90bec8d91382e5984a301dcdde8b67a90e
zoi <zoi@zoizoi.net>
2021-08-07 19:09:21 +0900

    UMLのEvent処理系のクラスを実装

---
+++ src/main/java/jp/hack/minecraft/hideandseek/event/EventManager.java
@@ -0,0 +1,4 @@
+package jp.hack.minecraft.hideandseek.event;
+
+public class EventManager {
+}
---
+++ src/main/java/jp/hack/minecraft/hideandseek/event/PlayerFreezeEvent.java
@@ -0,0 +1,11 @@
+package jp.hack.minecraft.hideandseek.event;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PlayerFreezeEvent extends Event {
+    @Override
+    public HandlerList getHandlers() {
+        return null;
+    }
+}
